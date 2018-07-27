8459b5aa13fbe9ad0c4a65a265c462364b504486
1970578978 <35584946+1970578978@users.noreply.github.com>
2018-07-27 16:19:10 +0800

    数据库配置

--- datasource/lib/config.php
+++ datasource/lib/config.php
@@ -2,9 +2,9 @@
 
 
 // 数据库配置
-$config['db']['host'] = '127.0.0.1';            //服务器120.79.211.61
+$config['db']['host'] = '120.79.211.61';            //服务器120.79.211.61
 $config['db']['username'] = 'root';
-$config['db']['password'] = '';         //服务器 890ccf33a9a4
+$config['db']['password'] = '890ccf33a9a4';         //服务器 890ccf33a9a4
 $config['db']['dbname'] = 'government';
 
 //百度云ai接口调用参数
